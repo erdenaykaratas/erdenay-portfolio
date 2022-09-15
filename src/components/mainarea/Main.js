d955d628d1e2b5fd0b7328371537fcd53a38f9b1
Erdenay Karataş <erdenay.karatas@icloud.com>
2022-09-15 21:10:38 +0300

    little adjustments

--- src/components/mainarea/Main.js
+++ src/components/mainarea/Main.js
@@ -5,13 +5,12 @@ import hello from "./Hi.png"
 const Main = () => {
     return (
         <div className="w-full h-screen overflow-hidden bg-[#f8f7f3]">
-        <div className="w-full h-80 overflow-hidden bg-[#f8f7f3]">
-            <img src={helloHand} alt="Greetings hand" className="w-2/3 absolute right-0 mt-32" />
-            <img src={hello} alt="Macbook" className="w-2/3 m-auto mt-20 md:mt-12" />
+        <div className="w-full h-96 overflow-hidden bg-[#f8f7f3]">
+            <img src={helloHand} alt="Greetings hand" className="w-2/3 md:w-1/2 absolute right-0 mt-32" />
+            <img src={hello} alt="Macbook" className="w-2/3 m-auto mt-20 z-10 md:mt-6" />
         </div>
-        <h3 className="w-4/5 m-auto mt-16 md:opacity-0 font-body text-xs flex justify-center items-center">Would you like to learn something about me ?</h3>
         <div className="w-20 h-20 bg-transparent absolute bottom-5 left-1/2">
-            <span className="absolute top-0 left-0 block w-full h-full -translate-y-20 -translate-x-1/2 rotate-45 border-b-4  border-r-4 border-solid border-[#737373] animate-bumpy"></span>
+            <a href="#" className="absolute top-0 left-0 block w-full h-full -translate-y-20 -translate-x-1/2 rotate-45 border-b-4  border-r-4 border-solid border-[#737373] animate-bumpy"></a>
         </div>
         </div>
     )
