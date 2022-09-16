302c4b51bc1a1ab5d782b60a12bd069aa0102b34
Erdenay Karataş <erdenay.karatas@icloud.com>
2022-09-16 21:10:00 +0300

    Little Adjustments v2

--- src/components/aboutme/AboutMe.js
+++ src/components/aboutme/AboutMe.js
@@ -5,14 +5,14 @@ import coffee from "./coffee.png"
 
 const AboutMe = () => {
     return (
-        <div className="w-full h-screen overflow-hidden relative">
+        <div className="w-full h-screen overflow-hidden relative bg-[#545454]">
             <div className="w-2/3 m-auto font-body text-2xl mb-4">
                 <h1 className="text-[#545E75]">Who am i ?</h1>
             </div>
             <div className="w-1/3 overflow-hidden">
-                <img src={hand2} className="w-1/2 absolute left-0 bottom-20" />
+                <img src={hand2} className="w-1/2 absolute left-0 bottom-12" />
             </div>
-            <div className="w-2/3 m-auto font-body text-[#] text-sm flex">
+            <div className="w-2/3 m-auto font-body text-sm flex">
                 <p className="text-[#545E75]">First of all, my name is Erdenay Karataş. I am a 21 years old computer programming student at Dokuz Eylül University. When i was graduated highscool, i had to choose a road for my life and then i met someone who has been coding for few years and i was impressed of his story. That moment i said to myself: "Hey, why not ?". So i started learning html, css and javascript with the guidance of my mentor and i liked it.Then i decided to be a web developer after i saw what and how i can build amazing things on web. So here is my story of how to start coding. I hope i can build something matters to the world and people one day.</p>
             </div>
         </div>
