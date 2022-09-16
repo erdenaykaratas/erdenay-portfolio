05514ef72acac0d83b7a0bd728188e58daaedf54
Erdenay Karataş <erdenay.karatas@icloud.com>
2022-09-16 09:45:59 +0300

    Little adjustments

--- src/components/aboutme/AboutMe.js
+++ src/components/aboutme/AboutMe.js
@@ -1,18 +1,19 @@
 import React from "react"
 import hand1 from "./aboutme1.png"
-import hand2 from "./aboutme2.png"
+import hand2 from "./about-me-img.png"
+import coffee from "./coffee.png"
 
 const AboutMe = () => {
     return (
-        <div className="w-full h-screen ">
+        <div className="w-full h-screen overflow-hidden relative">
             <div className="w-2/3 m-auto font-body text-2xl mb-4">
-                <h1>Who am i ?</h1>
+                <h1 className="text-[#545E75]">Who am i ?</h1>
             </div>
-            <div>
-                <img src={hand1} className="w-32 absolute -rotate-90 right-0" />
+            <div className="w-1/3 overflow-hidden">
+                <img src={hand2} className="w-1/2 absolute left-0 bottom-20" />
             </div>
-            <div className="w-2/3 m-auto font-body text-sm flex">
-                <p>First of all, my name is Erdenay Karataş. I am a 21 years old computer programming student at Dokuz Eylül University. When i was graduated highscool, i had to choose a road for my life and then i met someone who has been coding for few years and i was impressed of his story. That moment i said to myself: "Hey, why not ?". So i started learning html, css and javascript with the guidance of my mentor and i liked it.Then i decided to be a web developer after i saw what and how i can build amazing things on web. So here is my story of how to start coding. I hope i can build something matters to the world and people one day.</p>
+            <div className="w-2/3 m-auto font-body text-[#] text-sm flex">
+                <p className="text-[#545E75]">First of all, my name is Erdenay Karataş. I am a 21 years old computer programming student at Dokuz Eylül University. When i was graduated highscool, i had to choose a road for my life and then i met someone who has been coding for few years and i was impressed of his story. That moment i said to myself: "Hey, why not ?". So i started learning html, css and javascript with the guidance of my mentor and i liked it.Then i decided to be a web developer after i saw what and how i can build amazing things on web. So here is my story of how to start coding. I hope i can build something matters to the world and people one day.</p>
             </div>
         </div>
     )
--- src/components/mainarea/Main.js
+++ src/components/mainarea/Main.js
@@ -10,7 +10,7 @@ const Main = () => {
             <img src={hello} alt="Macbook" className="w-4/5 m-auto mt-20 z-10 md:mt-6" />
         </div>
         <div className="w-16 h-16 md:w-20 md:h-20 bg-transparent absolute bottom-5 left-1/2" >
-            <a href="#about-me" className="absolute top-0 left-0 block w-full h-full -translate-y-20 -translate-x-1/2 rotate-45 border-b-4  border-r-4 border-solid border-[#737373] animate-bumpy"></a>
+            <a href="#about-me" className="absolute top-0 left-0 mt-12 block w-full h-full -translate-y-20 -translate-x-1/2 rotate-45 border-b-4  border-r-4 border-solid border-[#737373] animate-bumpy"></a>
         </div>
         <div className="absolute bottom-0" id="about-me"></div>
         </div>
